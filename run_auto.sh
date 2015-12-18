2c617a09a91adf648ad9f1ba418fa7587ef59ca4
varung <varung@gmail.com>
2015-12-17 16:59:17 -0800

    better comments

--- run_auto.sh
+++ run_auto.sh
@@ -7,10 +7,12 @@ go build .
 ##PUT USER AND AUTH TOKEN HERE IN PLACE OF THESE SAMPLE CREDS##
 USER_TOKEN=something
 ACCESS_TOKEN=something
-URL=http://www.somedomain.com/api/v0.2/
+DOMAIN=zooxterminalcloud.com # e.g., terminal.com, ccterminal.com
+URL=http://www.$DOMAIN/api/v0.2/
+
 # don't shrink for 1 hour after starting
 TIME_TO_SHRINK=3600
-NODE_TYPE=g2.2xlarge
-NODE_TYPE_RAM=15000
+NODE_TYPE=g2.2xlarge # c4.8xlarge, c4.4xlarge
+NODE_TYPE_RAM=15000 # 60000, 30000 # units = megabytes
 
 ./autoscaler -usertoken=$UT -accesstoken=$AT -apiurl=$URL -policy=gpu -tts=$TIME_TO_SHRINK -nodetype=$NODE_TYPE -nodetyperam=$NODE_TYPE_RAM
